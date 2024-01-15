04444e9fb379efd1b3c3245698a58092b8fece40
Fellipe Marcel <fmrclms@gmail.com>
2024-01-15 13:19:07 -0300

    feat: implements BookController

--- src/main/java/com/betrybe/alexandria/controllers/BookController.java
+++ src/main/java/com/betrybe/alexandria/controllers/BookController.java
@@ -4,10 +4,17 @@ import com.betrybe.alexandria.controllers.dto.BookDTO;
 import com.betrybe.alexandria.controllers.dto.ResponseDTO;
 import com.betrybe.alexandria.models.entities.Book;
 import com.betrybe.alexandria.services.BookService;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +35,59 @@ public class BookController {
     Book newBook = bookService.insertBook(bookDTO.toBook());
     ResponseDTO<Book> responseDTO = new ResponseDTO<>("Livro criado com sucesso!", newBook);
     return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+  }
+
+  @PutMapping("/{bookId}")
+  public ResponseEntity<ResponseDTO<Book>> updateBook(@PathVariable Long bookId,
+      @RequestBody BookDTO bookDTO) {
+    Optional<Book> optionalBook = bookService.updateBook(bookId, bookDTO.toBook());
+
+    if (optionalBook.isEmpty()) {
+      ResponseDTO<Book> responseDTO = new ResponseDTO<>(
+          String.format("Livro com id %d não encontrado!", bookId), null);
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDTO);
+    }
+
+    ResponseDTO<Book> responseDTO = new ResponseDTO<>("Livro atualizado com sucesso!",
+        optionalBook.get());
+    return ResponseEntity.ok(responseDTO);
+  }
+
+  @DeleteMapping("/{bookId}")
+  public ResponseEntity<ResponseDTO<Book>> deleteBook(@PathVariable Long bookId) {
+    Optional<Book> optionalBook = bookService.removeBookById(bookId);
+
+    if (optionalBook.isEmpty()) {
+      ResponseDTO<Book> responseDTO = new ResponseDTO<>(
+          String.format("Livro com id %d não encontrado!", bookId), null);
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDTO);
+    }
+
+    ResponseDTO<Book> responseDTO = new ResponseDTO<>("Livro removido com sucesso!",
+        optionalBook.get());
+    return ResponseEntity.ok(responseDTO);
+  }
+
+  @GetMapping("/{bookId}")
+  public ResponseEntity<ResponseDTO<Book>> getBookById(@PathVariable Long bookId) {
+    Optional<Book> optionalBook = bookService.getBookById(bookId);
+
+    if (optionalBook.isEmpty()) {
+      ResponseDTO<Book> responseDTO = new ResponseDTO<>(
+          String.format("Livro com id %d não encontrado!", bookId), null);
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDTO);
+    }
+
+    ResponseDTO<Book> responseDTO = new ResponseDTO<>("Livro encontrado com sucesso!",
+        optionalBook.get());
+    return ResponseEntity.ok(responseDTO);
+  }
+
+  @GetMapping()
+  public List<BookDTO> getAllBooks() {
+    List<Book> books = (List<Book>) bookService.getAllBooks();
+    return books.stream()
+        .map(book -> new BookDTO(book.getId(), book.getTitle(), book.getGenre()))
+        .collect(Collectors.toList());
   }
 }
