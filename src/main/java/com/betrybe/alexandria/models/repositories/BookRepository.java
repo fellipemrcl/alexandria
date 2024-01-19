068eb74e1d9c85342360eac8c9e4739f0915d774
Fellipe Marcel <fmrclms@gmail.com>
2024-01-19 15:30:11 -0300

    feat: implements pagination to BookRepository, BookService and BookController

--- src/main/java/com/betrybe/alexandria/models/repositories/BookRepository.java
+++ src/main/java/com/betrybe/alexandria/models/repositories/BookRepository.java
@@ -3,8 +3,10 @@ package com.betrybe.alexandria.models.repositories;
 import com.betrybe.alexandria.models.entities.Author;
 import com.betrybe.alexandria.models.entities.Book;
 import com.betrybe.alexandria.models.entities.Publisher;
+import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,4 +16,6 @@ public interface BookRepository extends JpaRepository<Book, Long> {
   // Optional<Book> findByPublisher(Publisher publisher);
 
   // List<Book> findBookByAuthorsName(Author author);
+
+  Page<Book> findAll(Pageable pageable);
 }
